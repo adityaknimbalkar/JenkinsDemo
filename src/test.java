330dd8cd77ed85edb4674baee6c22e9b3f797b57
Adi <adityak.nimbalkar@gmail.com>
2023-10-29 19:02:07 +0530

    this new commit

---
+++ src/test.java
@@ -0,0 +1,18 @@
+public class test {
+    int a=0,b=0;
+    test(int a,int b){
+        this.a=a;
+        this.b=b;
+    }
+
+    public int addAndReturn(){
+        return this.a+this.b;
+    }
+
+    public static  void main(String args[]){
+
+        test t=new test(10,2222);
+        System.out.println(t.addAndReturn());
+        System.out.println("this is test function");
+    }
+}
